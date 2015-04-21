ef6f51be166c180078b6572e5291b7d6c4dc9e72
Evan Bender <evan.bender@percolate.com>
2015-04-21 14:46:09 -0400

    WIP but it fuckin works

--- interop/hlconsumer.py
+++ interop/hlconsumer.py
@@ -1,9 +1,50 @@
+#!/usr/bin/env python
+
 # export CLASSPATH=/kafka/libs/*
+
+# bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 4 --topic test
+
+import sys
+
 from jnius import autoclass
 
 # https://cwiki.apache.org/confluence/display/KAFKA/Consumer+Group+Example
+Consumer = autoclass("kafka.consumer.Consumer")
 ConsumerConfig = autoclass("kafka.consumer.ConsumerConfig")
 KafkaStream = autoclass("kafka.consumer.KafkaStream")
 ConsumerConnector = autoclass("kafka.javaapi.consumer.ConsumerConnector")
 
-print 'hi'
+Properties = autoclass("java.util.Properties")
+HashMap = autoclass("java.util.HashMap")
+Integer = autoclass("java.lang.Integer")
+
+
+def create_consumer_config(a_zookeeper, a_group_id):
+    props = Properties()
+    props.put("zookeeper.connect", a_zookeeper)
+    props.put("group.id", a_group_id)
+    props.put("zookeeper.session.timeout.ms", "400")
+    props.put("zookeeper.sync.time.ms", "200")
+    props.put("auto.commit.interval.ms", "1000")
+    return ConsumerConfig(props)
+
+
+def create_consumer(config):
+    return Consumer.createJavaConsumerConnector(config)
+
+if __name__ == "__main__":
+
+    topic = sys.argv[1]
+    config = create_consumer_config("localhost:2181", "test-group")
+    consumer = create_consumer(config)
+
+    topics_map = HashMap()
+    topics_map.put(topic, Integer(1))
+
+    consumer_map = consumer.createMessageStreams(topics_map)
+    streams = consumer_map.get(topic)
+    stream = streams.get(0)  # just going simple to start
+    it = stream.iterator()
+
+    while it.hasNext():
+        print it.next().message().tostring()
